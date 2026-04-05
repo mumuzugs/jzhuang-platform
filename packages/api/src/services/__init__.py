f99add748f79f32e18e7161d9e18d6a80933514e
mumuzugs <tuzhi126@163.com>
2026-04-05 23:28:58 +0800

    chore: 更新服务初始化

--- packages/api/src/services/__init__.py
+++ packages/api/src/services/__init__.py
@@ -1,2 +1,8 @@
 """服务层"""
 from src.services.sms import sms_service, send_sms_code, verify_sms_code
+from src.services.inspection import inspection_service, ISSUE_TYPES, CITY_RISKS
+from src.services.design import design_service, STYLES, LAYOUTS
+from src.services.budget import budget_service
+from src.services.construction import construction_service
+from src.services.supervision import cloud_supervision_service
+from src.services.payment import payment_service
